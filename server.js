99a93144410d4e293181f667efb7e0a45bf19ae7
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-22 22:33:13 +0000

    returning content from server using get request

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Handle GET request to fetch users
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Bob Williams" },
+    { id: 3, name: "Shannon Jackson" },
+  ];
+  res.send(`
+    <h1 class="text-2xl font-bold my-4">Users</h1>
+    <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join("")}
+    </ul>
+  `);
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
